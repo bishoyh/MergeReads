8998935876902ec79d579734e03519711269f1e8
Bishoy Hanna <bishoyh@gmail.com>
2019-06-04 15:19:29 -0600

    migration to python3

--- mergereads.py
+++ mergereads.py
@@ -4,20 +4,20 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import sys
-import itertools
+#import itertools
 
 ForwardName = str(sys.argv[1])  # forward reads file
 ReverseName = str(sys.argv[2])  # reverse reads file
 mergedName = str(sys.argv[3])  # output file
-print "Forward Name is " + ForwardName
-print "Reverse Name is " + ReverseName
-print "Merged output is " + mergedName
+print ("Forward Name is " + ForwardName)
+print ("Reverse Name is " + ReverseName)
+print ("Merged output is " + mergedName)
 
 insert = "N" * 300  # assumed insert size TODO pass as argument Crazy TODO add random DNA instead of NNNN
 
 
 def merge(iter1, iter2):
-    for (forward, reverse) in itertools.izip(iter1, iter2):
+    for (forward, reverse) in zip(iter1, iter2):
         assert forward.id == reverse.id
     allSeq = str(forward.seq + insert + reverse.seq.reverse_complement())
     seqfasta = SeqRecord(Seq(allSeq), id=forward.id) # Choosing to go with forward ID as the fasta header for, might parse it out in the future.
@@ -30,4 +30,4 @@ records_r = SeqIO.parse(open(ReverseName,"rU"), 'fastq') # Input format can be c
 handle = open(mergedName, "w")
 count = SeqIO.write(merge(records_f, records_r), handle, 'fasta') #Fastq won't work because the quality data is lost
 handle.close()
-print "Wrote %i entried to  %s" % (count, file_out)
+print ("Wrote %i entried to  %s" % (count, mergedName))
